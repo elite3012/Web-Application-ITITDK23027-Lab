6e4b0b8a293f44ce3b712a1bc3ab9f0fb1799358
Quys Tran <quyphuctran1@gmail.com>
2025-10-11 09:35:38 +0700

    fetch: add-lab1

---
+++ Lab1/TranPhucQuy_ITITDK23027.sql
@@ -0,0 +1,42 @@
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(100),
+    DoB DATE,
+    Major VARCHAR(100)
+);
+
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100)
+);
+
+CREATE TABLE Register (
+    StudentID INT,
+    CourseID INT,
+    PRIMARY KEY (StudentID, CourseID),
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+);
+
+CREATE TABLE Lecturer (
+    LecturerID INT PRIMARY KEY,
+    LecturerName VARCHAR(100),
+    DepartmentID INT
+);
+
+CREATE TABLE Teach (
+    LecturerID INT,
+    CourseID INT,
+    PRIMARY KEY (LecturerID, CourseID),
+    FOREIGN KEY (LecturerID) REFERENCES Lecturer(LecturerID),
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+);
+
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100)
+);
+
+ALTER TABLE Lecturer
+ADD FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID);
